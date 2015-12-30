bdcf7cd1d3018aac490949477d46cfb3b8dfe2cf
LiuEcho <lc7906@qq.com>
2015-12-31 00:02:15 +0800

    1.0

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var express = require('express')//引入express模块
-	app = express()
-	server = require('http').createServer(app)
+var express = require('express'),//引入express模块
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 app.use('/', express.static(__dirname + '/www'))//指定静态HTML文件的位置
-server.listen(8080)
+server.listen(3000)
 console.log("server started")
+
+io.on('connection', function(socket) {
+    //接收并处理客户端发送的foo事件
+    socket.on('foo', function(data) {
+        //将消息输出到控制台
+        console.log(data);
+    })
+});
